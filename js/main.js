5e4ccf5e364e8a15fc29946141f110c29a841d88
Alison Case <alison.case@gmail.com>
2025-03-13 17:29:50 -0400

    Adjusting symbology based on user testing feedback

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
 /* Map of country roadmap data */
 
 // Initialize the map
-var map = L.map('map').setView([20, 0], 2);
+var map = L.map('map', {
+    maxBounds: [[-90, -180], [90, 180]], // Set max bounds to the global view
+    maxBoundsViscosity: 1.0 // Prevent the user from zooming out beyond the global view
+}).setView([20, 0], 2);
 
 // Add base tilelayer
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
     attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
     maxZoom: 16,
-    ext: 'png'
+    ext: 'png',
+    noWrap: true // Prevent the user from zooming out beyond the global view
 }).addTo(map);
 
 var geojsonLayer;
 var proportionalSymbolsLayer;
 var bivariateLayer;
 var colorScale = d3.scaleQuantize().range(d3.schemeBlues[5]);
-var currentIndicator = 'capacity'; // Default metric on map load
+var currentIndicator = 'Capacity'; // Default metric on map load
 var proportionalLegend; // Legend for proportional symbols
 var openPopup; // Variable to store the currently open popup
 
@@ -42,17 +46,17 @@ d3.csv("data/roadmap_data_fy25.csv").then(function(data) {
     // Define indicators for each container
     var container0Indicators = ['capacity_commitment'];
     var container1Indicators = [
-        'commitment', 'liberal_democracy', 'corruption', 'open_government', 
-        'social_group_equality', 'economic_gender_gap', 'business_envt', 
-        'trade_freedom', 'environmental_policy'
+        'Commitment', 'Liberal Democracy', 'Absence of Corruption', 'Open Government', 
+        'Social Group Equality', 'Economic Gender Gap', 'Business & Investment Environment', 
+        'Trade Freedom', 'Environmental Policy'
     ];
     var container2Indicators = [
-        'capacity', 'gov_effectiveness', 'tax_system_effectiveness', 
-        'safety_security', 'cs_media_effectiveness', 'poverty_rate', 
-        'edu_quality', 'child_health', 'gdp_per_capita', 'mobile_connectivity', 
-        'export_sophistication'
+        'Capacity', 'Government Effectiveness', 'Tax System Effectiveness', 
+        'Safety & Security', 'Civil Society & Media Effectiveness', 'Poverty Rate ($6.85/Day)', 
+        'Education Quality', 'Child Health', 'GDP Per Capita (PPP)', 'Mobile Connectivity', 
+        'Export Sophistication'
     ];
-    var container3Indicators = ['debt_risk', 'fragility'];
+    var container3Indicators = ['Risk of External Debt Distress', 'Fragility'];
 
     // Dynamically generate buttons for each metric in the defined containers
     container0Indicators.forEach(indicator => {
@@ -178,16 +182,17 @@ d3.csv("data/roadmap_data_fy25.csv").then(function(data) {
             // Add legend title
             labels.push('<strong>' + currentIndicator.replace(/_/g, ' ').charAt(0).toUpperCase() + currentIndicator.replace(/_/g, ' ').slice(1) + '</strong>');
 
-            // Create a legend label with a different color for each quantile interval of the indicator
-            for (var i = 0; i < grades.length; i++) {
-                var from = grades[i];
-                var to = grades[i + 1];
+            // Add "Worst Outcome" label
+            labels.push('<i style="background:' + colorScale(grades[0]) + '"></i> Best Outcome');
 
-                labels.push(
-                    '<i style="background:' + colorScale(from) + '"></i> ' +
-                    from.toFixed(2) + (to ? '&ndash;' + to.toFixed(2) : '+')
-                );
+            // Add color squares for intermediate values without labels
+            for (var i = 1; i < grades.length - 1; i++) {
+                var from = grades[i];
+                labels.push('<i style="background:' + colorScale(from) + '"></i>');
             }
+
+            // Add "Best Outcome" label
+            labels.push('<i style="background:' + colorScale(grades[grades.length - 1]) + '"></i> Worst Outcome');
 
             // Add a label for "N/A" with no fill
             labels.push('<i style="background:none; border:1px solid #ccc;"></i> N/A');
@@ -237,16 +242,17 @@ d3.csv("data/roadmap_data_fy25.csv").then(function(data) {
                 // Add legend title
                 labels.push('<strong>' + currentIndicator.replace(/_/g, ' ').charAt(0).toUpperCase() + currentIndicator.replace(/_/g, ' ').slice(1) + '</strong>');
 
-                // Update colored square labels based on the selected indicator
-                for (var i = 0; i < grades.length; i++) {
-                    var from = grades[i];
-                    var to = grades[i + 1];
+                // Add "Worst Outcome" label
+                labels.push('<i style="background:' + colorScale(grades[0]) + '"></i> Best Outcome');
 
-                    labels.push(
-                        '<i style="background:' + colorScale(from) + '"></i> ' +
-                        from.toFixed(2) + (to ? '&ndash;' + to.toFixed(2) : '+')
-                    );
+                // Add color squares for intermediate values without labels
+                for (var i = 1; i < grades.length - 1; i++) {
+                    var from = grades[i];
+                    labels.push('<i style="background:' + colorScale(from) + '"></i>');
                 }
+
+                // Add "Best Outcome" label
+                labels.push('<i style="background:' + colorScale(grades[grades.length - 1]) + '"></i> Worst Outcome');
 
                 // Add a label for "N/A" with no fill
                 labels.push('<i style="background:none; border:1px solid #ccc;"></i> N/A');
@@ -274,8 +280,8 @@ d3.csv("data/roadmap_data_fy25.csv").then(function(data) {
             var commitmentValues = [];
             for (let i = 0; i < data.length; i++) {
                 var dataPoint = data[i];
-                capacityValues.push(parseFloat(dataPoint['capacity']));
-                commitmentValues.push(parseFloat(dataPoint['commitment']));
+                capacityValues.push(parseFloat(dataPoint['Capacity']));
+                commitmentValues.push(parseFloat(dataPoint['Commitment']));
             }
             capacityValues.sort(d3.ascending);
             commitmentValues.sort(d3.ascending);
@@ -300,8 +306,8 @@ d3.csv("data/roadmap_data_fy25.csv").then(function(data) {
 
             function bivariateStyle(feature) {
                 var countryData = data.find(d => d.country === feature.properties.WP_Name);
-                var capacity = countryData ? countryData['capacity'] : null;
-                var commitment = countryData ? countryData['commitment'] : null;
+                var capacity = countryData ? countryData['Capacity'] : null;
+                var commitment = countryData ? countryData['Commitment'] : null;
                 var capacityQuintile = capacity !== null ? capacityQuintiles(capacity) : null;
                 var commitmentQuintile = commitment !== null ? commitmentQuintiles(commitment) : null;
                 var mappedCapacityQuintile = capacityQuintile !== null ? mapToThree(capacityQuintile) : null;
@@ -329,8 +335,8 @@ d3.csv("data/roadmap_data_fy25.csv").then(function(data) {
                             });
 
                             var countryData = data.find(d => d.country === layer.feature.properties.WP_Name);
-                            var capacity = countryData ? countryData['capacity'] : 'N/A';
-                            var commitment = countryData ? countryData['commitment'] : 'N/A';
+                            var capacity = countryData ? countryData['Capacity'] : 'N/A';
+                            var commitment = countryData ? countryData['Commitment'] : 'N/A';
                             var popupContent = `<b>${layer.feature.properties.WP_Name}</b><br>Capacity: ${capacity}<br>Commitment: ${commitment}`;
                             layer.bindPopup(popupContent).openPopup();
                             openPopup = layer.getPopup(); // Store the currently open popup
@@ -359,14 +365,17 @@ d3.csv("data/roadmap_data_fy25.csv").then(function(data) {
                 // Add legend title
                 labels.push('<strong>Capacity and Commitment</strong>');
 
-                // Update colored square labels based on the bivariate indicator
+                // Add "Worst Outcome" label
+                labels.push('<i style="background:' + bivariateColorScale("0,0") + '"></i> Worst Outcome');
+
+                // Add color squares for intermediate values without labels
                 var colors = bivariateColorScale.range().reverse();
-                for (var i = 0; i < colors.length; i++) {
-                    labels.push(
-                        '<i style="background:' + colors[i] + '"></i> ' +
-                        ((colors.length - 1 - i) / (colors.length - 1)).toFixed(2)
-                    );
+                for (var i = 1; i < colors.length - 1; i++) {
+                    labels.push('<i style="background:' + colors[i] + '"></i>');
                 }
+
+                // Add "Best Outcome" label
+                labels.push('<i style="background:' + bivariateColorScale("2,2") + '"></i> Best Outcome');
 
                 // Add a label for "N/A" with no fill
                 labels.push('<i style="background:none; border:1px solid #ccc;"></i> N/A');
@@ -401,12 +410,12 @@ d3.csv("data/roadmap_data_fy25.csv").then(function(data) {
                 var color = "#ff7800";
                 var label = value;
 
-                if (indicator === 'fragility') {
-                    if (value === null) {
-                        radius = 0;
-                        color = "#ccc";
-                        label = 'N/A';
-                    } else if (value <= 30) {
+                if (value === null) {
+                    return; // Skip countries with null values
+                }
+
+                if (indicator === 'Fragility') {
+                    if (value <= 30) {
                         radius = 5;
                         color = "#fee5d9";
                         label = 'Sustainable';
@@ -423,12 +432,8 @@ d3.csv("data/roadmap_data_fy25.csv").then(function(data) {
                         color = "#cb181d"; 
                         label = 'Alert';
                     }
-                } else if (indicator === 'debt_risk') {
-                    if (value === null) {
-                        radius = 0;
-                        color = "#ccc";
-                        label = 'N/A';
-                    } else if (value == 1) {
+                } else if (indicator === 'Risk of External Debt Distress') {
+                    if (value == 1) {
                         radius = 5;
                         color = "#fee5d9"; 
                         label = 'Low';
@@ -473,6 +478,12 @@ d3.csv("data/roadmap_data_fy25.csv").then(function(data) {
                 }
             });
 
+            proportionalSymbolsLayer.eachLayer(function(layer) {
+                if (layer.options.radius === 0) {
+                    proportionalSymbolsLayer.removeLayer(layer); // Remove markers with radius 0
+                }
+            });
+
             proportionalSymbolsLayer.addTo(map);
 
             // Update legend for proportional symbols
@@ -486,14 +497,14 @@ d3.csv("data/roadmap_data_fy25.csv").then(function(data) {
                 labels.push('<strong>' + currentIndicator.replace(/_/g, ' ').charAt(0).toUpperCase() + currentIndicator.replace(/_/g, ' ').slice(1) + '</strong>');
 
                 // Update colored square labels based on the selected indicator
-                if (indicator === 'fragility') {
+                if (indicator === 'Fragility') {
                     labels.push(
                         '<i style="background:#cb181d"></i> Alert',
                         '<i style="background:#fb6a4a"></i> Warning',
                         '<i style="background:#fcae91"></i> Stable',
                         '<i style="background:#fee5d9"></i> Sustainable'
                     );
-                } else if (indicator === 'debt_risk') {
+                } else if (indicator === 'Risk of External Debt Distress') {
                     labels.push(
                         '<i style="background:#cb181d"></i> In debt distress',
                         '<i style="background:#fb6a4a"></i> High',
@@ -501,6 +512,9 @@ d3.csv("data/roadmap_data_fy25.csv").then(function(data) {
                         '<i style="background:#fee5d9"></i> Low'
                     );
                 }
+
+                // Add a label for "N/A" with no fill
+                labels.push('<i style="background:none; border:1px solid #ccc;"></i> N/A');
 
                 div.innerHTML = labels.join('<br>');
                 return div;
